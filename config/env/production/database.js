a4e3a5c3648fa83d891a23622cecfde6b0f6d604
mobydeck <mo.mubark@yahoo.com>
2020-10-08 18:10:13 +0200

    after update env for production on aws

--- config/env/production/database.js
+++ config/env/production/database.js
@@ -17,6 +17,7 @@
     },
 });
 */
+/*
 module.exports = ({ env }) => ({
     defaultConnection: 'default',
     connections: {
@@ -29,6 +30,27 @@ module.exports = ({ env }) => ({
                 database: env('DATABASE_NAME', 'strapi'),
                 username: env('DATABASE_USERNAME', ''),
                 password: env('DATABASE_PASSWORD', ''),
+                schema: 'public',
+            },
+            options: {},
+        },
+    },
+});
+
+*/
+
+module.exports = ({ env }) => ({
+    defaultConnection: 'default',
+    connections: {
+        default: {
+            connector: 'bookshelf',
+            settings: {
+                client: 'postgres',
+                host: env('DATABASE_HOST', 'localhost'),
+                port: env.int('DATABASE_PORT', 5432),
+                database: env('DATABASE_NAME', 'postgres'),
+                username: env('DATABASE_USERNAME', 'Sayed'),
+                password: env('DATABASE_PASSWORD', 'Sayed2020'),
                 schema: 'public',
             },
             options: {},
